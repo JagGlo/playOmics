3a263094389eb0ad9beabf9e5fb7bba7f8252e90
Jagoda Głowacka-Walas <jagaaa92@gmail.com>
2023-08-23 13:51:25 +0200

    GUI v1.1, including modules

---
+++ R/module-prediction.R
@@ -0,0 +1,117 @@
+predictionUI <- function(id) {
+  ns <- NS(id)
+
+  tagList(
+    shiny::tags$div(
+      fluidPage(
+        shiny::radioButtons(ns("select_input"),
+          label = "Select data for prediction",
+          choices = c("Insert values", "Select data from your env", "Load data from file"),
+          selected = "Insert values"
+        ),
+        uiOutput(ns("predict_dashboard")),
+        htmlOutput(ns("this_is_prediction")),
+        DT::dataTableOutput(ns("show_prediction")),
+        htmlOutput(ns("this_is_explanation")),
+        plotOutput(ns("explain"))
+      )
+    )
+  )
+}
+
+predictionServer <- function(id, df, values) {
+  moduleServer(id, function(input, output, session) {
+    output$predict_dashboard <- renderUI({
+      tagList(
+        fluidPage(
+          if (input$select_input == "Insert values") {
+            names <- na.omit(results$model_name[results$model_dir == values$model_link])
+            vars <-
+              names %>%
+              strsplit(split = "\\s\\+\\s") %>%
+              unlist()
+            lapply(1:length(vars), function(i) {
+              numericInput(session$ns(paste0("input", i)), vars[i], "")
+            })
+          } else if (input$select_input == "Select data from your env") {
+            dataframes <- ls(envir = .GlobalEnv)[sapply(mget(ls(envir = .GlobalEnv), envir = .GlobalEnv), is.data.frame) | sapply(mget(ls(envir = .GlobalEnv), envir = .GlobalEnv), is.list)]
+            selectInput(session$ns("env_dataframe"), "Select a dataframe or a list:", dataframes)
+          }
+        ),
+        actionButton(session$ns("predict"), "Predict")
+      )
+    })
+
+    observeEvent(input$predict, {
+      if (input$select_input == "Insert values") {
+        # Extract list of data frames from reactiveValues
+        list_of_inputs <- reactiveValuesToList(input)
+
+        # Filter list by keys
+        filtered_list <- list_of_inputs[names(list_of_inputs) %in% grep("input\\d", names(list_of_inputs), value = TRUE)]
+
+        # If you need to bind them together into one data frame (assuming they have the same structure):
+        new_data <- bind_rows(filtered_list) %>% select(order(colnames(.)))
+
+        names <- na.omit(results$model_name[results$model_dir == values$model_link])
+        vars <-
+          names %>%
+          strsplit(split = "\\s\\+\\s") %>%
+          unlist()
+        colnames(new_data) <- vars
+      } else if (input$select_input == "Select data from your env") {
+        data_from_env <- get(input$env_dataframe, envir = .GlobalEnv)
+        new_data <-
+          if (!is.data.frame(data_from_env)) {
+            prepare_data_for_modelling(data_from_env)
+          } else {
+            data_from_env
+          }
+      }
+      values$prediction_data <- new_data
+    })
+
+    output$show_prediction <- renderDT({
+      req(!is.null(values$prediction_data))
+      predicted <- load_and_predict(values$model_link, values$prediction_data)
+      values$prediction_result <- predicted
+      values$prediction_result %>%
+        mutate_if(is.numeric, round, 3) %>%
+        datatable(
+          rownames = FALSE,
+          options = list(searching = FALSE, paging = FALSE, server = FALSE, escape = FALSE, selection = "none")
+        )
+    })
+
+    output$this_is_prediction <- renderUI({
+      req(!is.null(values$prediction_data))
+      h3("Prediction result(s):")
+    })
+
+    output$this_is_explanation <- renderUI({
+      req(!is.null(values$prediction_result))
+      h3("Explanation with SHAP values:")
+    })
+
+    output$explain <- renderPlot({
+      req(!is.null(values$prediction_result))
+      explained <- load_and_explain(values$model_link, values$prediction_data)
+
+      explained %>%
+        group_by(variable) %>%
+        mutate(mean_val = mean(contribution)) %>%
+        ungroup() %>%
+        mutate(variable = fct_reorder(variable, abs(mean_val))) %>%
+        ggplot(aes(contribution, variable, fill = mean_val > 0)) +
+        geom_col(
+          data = ~ distinct(., variable, mean_val),
+          aes(mean_val, variable),
+          alpha = 0.5
+        ) +
+        geom_boxplot(width = 0.5) +
+        theme_bw() +
+        theme(legend.position = "none") +
+        labs(y = NULL)
+    })
+  })
+}
--- R/show_results.R
+++ R/show_results.R
@@ -1,22 +1,52 @@
-results_GUI <- function(results){
-
 library(shiny)
 library(DT)
 library(plotly)
 
-shinyApp(
+remove_shiny_inputs <- function(id, .input) {
+  invisible(
+    lapply(grep(id, names(.input), value = TRUE), function(i) {
+      .subset2(.input, "impl")$.values$remove(i)
+    })
+  )
+}
+
+results_GUI <- function(results) {
   ui <- fluidPage(
+    column(uiOutput("choose_metrics"), width = 3),
     DT::dataTableOutput("data"),
-    DT::dataTableOutput("present_data"),
-    plotlyOutput("3dplot"),
-    DT::dataTableOutput("variable_stats"),
-    # plotOutput("variable_stats_plots"),
-    uiOutput("predict_dashboard"),
-    plotOutput("explain")
-  ),
+    tabsetPanel(
+      id = "tabset",
+      tabPanel(
+        "Experiment overview",
+        actionButton("add_plot", "Add new plot"),
+        verticalLayout(
+          div(id = "addPlaceholder"),
+          shinyjs::useShinyjs(debug = TRUE),
+        )
+      ),
+      tabPanel(
+        "Analytes overview",
+        DT::dataTableOutput("analytes_stats")
+      ),
+      tabPanel(
+        "Single model overview",
+        DT::dataTableOutput("present_data"),
+        plotlyOutput("3dplot"),
+        DT::dataTableOutput("variable_stats"),
+        # plotOutput("variable_stats_plots")
+      ),
+      tabPanel(
+        "Prediction",
+        predictionUI("new_data")
+      )
+      # tabPanel("Model ensembling"
+      #
+      # )
+    )
+  )
 
-  server <- function(input, output) {
 
+  server <- function(input, output, session) {
     values <- reactiveValues(df_data = NULL, model_link = NULL, prediction_data = NULL, prediction_result = NULL)
 
     shinyInput <- function(FUN, len, id, ...) {
@@ -27,39 +57,167 @@ shinyApp(
       inputs
     }
 
-    df <- reactiveValues(data = data.frame(
-      results[,c(1:14)],
-      Data = shinyInput(actionButton, nrow(results), 'button_', label = "Show data", onclick = 'Shiny.onInputChange(\"present_data_btn\",  this.id)' ),
-      Predict = shinyInput(actionButton, nrow(results), 'button_', label = "Make prediction", onclick = 'Shiny.onInputChange(\"predict_btn\",  this.id)' ),
-      stringsAsFactors = FALSE,
-      row.names = 1:nrow(results)
-    ))
-
-
-    output$data <- DT::renderDataTable(
-      df$data, server = FALSE, escape = FALSE, selection = 'none'
+    df <- reactiveValues(
+      data = data.frame(
+        results %>% mutate_if(is.numeric, round, 3),
+        Data = shinyInput(actionButton, nrow(results), "button_", label = "Show data", onclick = 'Shiny.onInputChange(\"present_data_btn\",  this.id)'),
+        Predict = shinyInput(actionButton, nrow(results), "button_", label = "Make prediction", onclick = 'Shiny.onInputChange(\"predict_btn\",  this.id)'),
+        stringsAsFactors = FALSE,
+        row.names = 1:nrow(results)
+      ) %>%
+        relocate(c(Data, Predict), .after = model_name) %>%
+        relocate(c(model_id, model_dir), .after = last_col())
     )
 
+    output$choose_metrics <- renderUI({
+      req(nrow(df$data) > 0)
+      shinyWidgets::pickerInput(
+        inputId = "select_metrics",
+        label = "Choose column to present:",
+        multiple = TRUE,
+        selected = names(df$data),
+        choices = names(df$data),
+        options = list(`actions-box` = TRUE)
+      )
+    })
+
+    # plotting module
+    list_modules <- list()
+    current_id <- 1 # current module id
+
+    # dynamically adding new fields
+    observeEvent(input$add_plot, {
+      id <- paste0("plot_", current_id)
+
+      insertUI(
+        selector = "#addPlaceholder",
+        where = "beforeEnd",
+        ui = plottingUI(session$ns(id),
+          box_id = current_id,
+          data = results %>% select(-c(model_name, model_id, model_dir))
+        )
+      )
+
+      plottingServer(id, results %>% select(-c(model_name, model_id, model_dir)))
+
+      observeEvent(input[[paste0(id, "-box")]][["visible"]], {
+        if (input[[paste0(id, "-box")]][["visible"]] == FALSE) {
+          removeUI(selector = paste0("#", id))
+          list_modules[[id]] <<- NULL
+          remove_shiny_inputs(paste0(id, "-box"), input)
+        }
+      })
+
+      current_id <<- current_id + 1
+    })
+
+    output$data <- DT::renderDT({
+      req(!is.null(input$select_metrics))
+
+      df$data %>%
+        select(input$select_metrics) %>%
+        DT::datatable(
+          extensions = c("Buttons", "Scroller"),
+          # filter = "top",
+          options = list(
+            # searching = FALSE,
+            columnDefs = list(list(className = "dt-left", targets = "_all")),
+            dom = "Bfrtip",
+            deferRender = TRUE,
+            scroller = TRUE,
+            scrollX = TRUE,
+            scrollY = 400,
+            buttons = c("copy", "csv", "excel", "pdf", "print")
+          ),
+          class = "cell-border strip hover",
+          escape = FALSE
+        )
+    })
+
+    output$analytes_stats <- DT::renderDT({
+      vars <- lapply(1:nrow(df$data), function(i) {
+        vars <-
+          df$data[i, "model_name"] %>%
+          strsplit(split = "\\s\\+\\s") %>%
+          unlist() %>%
+          as_tibble() %>%
+          rename(variable = value)
+
+        bind_cols(
+          model_name = df$data[i, "model_name"],
+          vars
+        )
+      }) %>%
+        bind_rows()
+
+
+      vars %>%
+        left_join(df$data, by = "model_name") %>%
+        group_by(variable) %>%
+        summarise(appears_in_n_models = n()) %>%
+        left_join(
+          vars %>%
+            left_join(df$data, by = "model_name") %>%
+            group_by(variable) %>%
+            summarise_if(is.numeric, mean, na.rm = T),
+          by = "variable"
+        ) %>%
+        mutate_if(is.numeric, round, 3) %>%
+        DT::datatable(
+          extensions = c("Buttons", "Scroller"),
+          # filter = "top",
+          options = list(
+            columnDefs = list(list(className = "dt-left", targets = "_all")),
+            dom = "Bfrtip",
+            deferRender = TRUE,
+            scroller = TRUE,
+            scrollX = TRUE,
+            scrollY = 400,
+            buttons = c("copy", "csv", "excel", "pdf", "print")
+          ),
+          class = "cell-border strip hover",
+          escape = FALSE
+        )
+    })
+
+
     observeEvent(input$present_data_btn, {
+      updateTabsetPanel(
+        session = getDefaultReactiveDomain(),
+        "tabset",
+        selected = "Single model overview"
+      )
+
       selectedRow <- as.numeric(strsplit(input$present_data_btn, "_")[[1]][2])
-      link_to_data <-  results$data[selectedRow]
+      link_to_data <- paste(results$model_dir[selectedRow], "train_data.Rds", sep = "/")
       values$df_data <<- readRDS(link_to_data)
     })
 
-    observeEvent(input$predict_btn, {
-      selectedRow <- as.numeric(strsplit(input$predict_btn, "_")[[1]][2])
-      link_to_data <-  results$model[selectedRow]
-      values$model_link <<- link_to_data
+    output$present_data <- DT::renderDT({
+      req(nrow(values$df_data) > 0)
+      values$df_data %>%
+        DT::datatable(
+          extensions = c("Buttons", "Scroller"),
+          options = list(
+            searching = FALSE,
+            columnDefs = list(list(className = "dt-left", targets = "_all")),
+            dom = "Bfrtip",
+            deferRender = TRUE,
+            scroller = TRUE,
+            scrollX = TRUE,
+            scrollY = 400,
+            buttons = c("copy", "csv", "excel", "pdf", "print")
+          ),
+          class = "cell-border strip hover"
+        )
     })
 
-    output$present_data <- DT::renderDataTable(
-      values$df_data, server = FALSE, escape = FALSE, selection = 'none'
-    )
-
     output$variable_stats <- DT::renderDT({
+      req(nrow(values$df_data) > 0)
       count_stats_per_model(values$df_data) %>%
-      datatable(filter="top",
-                options = list(server = FALSE, escape = FALSE, selection = 'none') )
+        datatable(
+          options = list(searching = FALSE, paging = FALSE, server = FALSE, escape = FALSE, selection = "none")
+        )
     })
 
     # output$variable_stats_plots <- renderPlot({
@@ -87,90 +245,42 @@ shinyApp(
     #
     # })
 
-    output$`3dplot`<-renderPlotly({
+    output$`3dplot` <- renderPlotly({
       req(input$present_data_btn)
-      if(length(values$df_data) == 3){
-          p <-
-            values$df_data %>%
-            ggplot(aes_(x = as.name(names(values$df_data)[1]), y =  as.name(names(values$df_data)[2]), color = as.name(target$target_variable))) +
-            geom_point() +
-            theme_bw()
-          ggplotly(p)
-
+      if (length(values$df_data) == 3) {
+        p <-
+          values$df_data %>%
+          ggplot(aes_(x = as.name(names(values$df_data)[1]), y = as.name(names(values$df_data)[2]), color = as.name(target$target_variable))) +
+          geom_jitter(width = 0.1, height = 0.1) +
+          theme_bw()
+        ggplotly(p)
       } else {
-          plotly::plot_ly(values$df_data,
-                          x = as.formula(paste0("~\u0060", names(values$df_data)[1], "\u0060")),
-                          y = as.formula(paste0("~\u0060", names(values$df_data)[2], "\u0060")),
-                          z = as.formula(paste0("~\u0060", names(values$df_data)[3], "\u0060")),
-                          color = as.formula(paste0("~", target$target_variable)),
-                          colors = c("#0C4B8E", "#BF382A"),
-                          type = "scatter3d",
-                          mode = "markers"
-          )
+        plotly::plot_ly(values$df_data,
+          x = as.formula(paste0("~\u0060", names(values$df_data)[1], "\u0060")),
+          y = as.formula(paste0("~\u0060", names(values$df_data)[2], "\u0060")),
+          z = as.formula(paste0("~\u0060", names(values$df_data)[3], "\u0060")),
+          color = as.formula(paste0("~", target$target_variable)),
+          colors = c("#0C4B8E", "#BF382A"),
+          type = "scatter3d",
+          mode = "markers"
+        )
       }
-
     })
 
-    output$predict_dashboard <- renderUI({
-      req(input$predict_btn)
-      names <- results$model_name[results$model == values$model_link]
-      vars <-
-        names %>%
-        strsplit(split = "\\s\\+\\s") %>%
-        unlist()
-      tagList(
-        shinydashboardPlus::box(
-          "Predict",
-          collapsible = TRUE,
-        numericInput("input1", vars[1],""),
-        numericInput("input2", vars[2],""),
-        numericInput("input3", vars[3], ""),
-        actionButton("predict", "Predict"),
-        DT::dataTableOutput("show_prediction")
+    observeEvent(input$predict_btn, {
+      updateTabsetPanel(
+        session = getDefaultReactiveDomain(),
+        "tabset",
+        selected = "Prediction"
       )
-      )
+
+      selectedRow <- as.numeric(strsplit(input$predict_btn, "_")[[1]][2])
+      link_to_model <- results$model_dir[selectedRow]
+      values$model_link <<- link_to_model
+
+      predictionServer("new_data", results, values)
     })
-
-    observeEvent(input$predict, {
-      prediction_data <- data.frame(input$input1,input$input2,input$input3)
-      names <- results$model_name[results$model == values$model_link]
-      vars <-
-        names %>%
-        strsplit(split = "\\s\\+\\s") %>%
-        unlist()
-      colnames(prediction_data) <- vars
-      values$prediction_data <- prediction_data
-       mymodel <- mlflow::mlflow_load_model(values$model_link)
-       predicted <- mlflow::mlflow_predict(mymodel, prediction_data)
-       values$prediction_result <- predicted
-    })
-
-      output$show_prediction <-renderDT({
-        values$prediction_result
-      })
-
-      output$explain <- renderPlot({
-        req(!is.null(values$prediction_result))
-        values$explainer_link <- stringr::str_replace(values$model_link, "model", "explainer")
-        myexplainer <- mlflow::mlflow_load_model(values$explainer_link)
-        explained <- mlflow::mlflow_predict(myexplainer, values$prediction_data)
-
-        explained %>%
-          group_by(variable) %>%
-          mutate(mean_val = mean(contribution)) %>%
-          ungroup() %>%
-          mutate(variable = fct_reorder(variable, abs(mean_val))) %>%
-          ggplot(aes(contribution, variable, fill = mean_val > 0)) +
-          geom_col(data = ~distinct(., variable, mean_val),
-                   aes(mean_val, variable),
-                   alpha = 0.5) +
-          geom_boxplot(width = 0.5) +
-          theme_bw() +
-          theme(legend.position = "none") +
-          labs(y = NULL)
-      })
   }
-)
 
-shinyApp(ui = ui, server = server)
+  shinyApp(ui = ui, server = server)
 }
